355e16c672aebcf468b01c3a57baf31cadfde7c8
velliss200 <velliss200@gmail.com>
2026-02-06 00:06:41 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+const AboutUsBtn = document.getElementById("AboutUsBtn");
+AboutUsBtn.addEventListener("click", () => {
+  window.location.href = "index.html";
+});
+const ServicesBtn = document.getElementById("ServicesBtn");
+ServicesBtn.addEventListener("click", () => {
+  window.location.href = "services.html";
+});
+const ContactsBtn = document.getElementById("ContactsBtn");
+ContactsBtn.addEventListener("click", () => {
+  window.location.href = "contacts.html";
+});
+const LearnMoreBtn = document.getElementById("LearnMoreBtn");
+LearnMoreBtn.addEventListener("click", () => {
+  window.location.href = "services.html";
+});
