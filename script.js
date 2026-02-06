6f71ef2081f03763240b380dd079963ea25b7e1c
velliss200 <velliss200@gmail.com>
2026-02-07 01:27:19 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,87 @@
-const AboutUsBtn = document.getElementById("AboutUsBtn");
-AboutUsBtn.addEventListener("click", () => {
-  window.location.href = "index.html";
+document.addEventListener("DOMContentLoaded", function () {
+  const AboutUsBtn = document.getElementById("AboutUsBtn");
+  if (AboutUsBtn)
+    AboutUsBtn.addEventListener("click", () => {
+      window.location.href = "index.html";
+    });
+
+  const ServicesBtn = document.getElementById("ServicesBtn");
+  if (ServicesBtn)
+    ServicesBtn.addEventListener("click", () => {
+      window.location.href = "services.html";
+    });
+
+  const ContactsBtn = document.getElementById("ContactsBtn");
+  if (ContactsBtn)
+    ContactsBtn.addEventListener("click", () => {
+      window.location.href = "contacts.html";
+    });
+
+  const LearnMoreBtn = document.getElementById("LearnMoreBtn");
+  if (LearnMoreBtn)
+    LearnMoreBtn.addEventListener("click", () => {
+      window.location.href = "services.html";
+    });
+
+  const lang = document.getElementById("lang");
+  if (lang) {
+    lang.addEventListener("change", function () {
+      if (this.value === "english") {
+        window.location.href = "index.html";
+      }
+      if (this.value === "bulgarian") {
+        window.location.href = "indexBg.html";
+      }
+      if (this.value === "russian") {
+        window.location.href = "indexRu.html";
+      }
+    });
+  }
 });
-const ServicesBtn = document.getElementById("ServicesBtn");
-ServicesBtn.addEventListener("click", () => {
-  window.location.href = "services.html";
-});
-const ContactsBtn = document.getElementById("ContactsBtn");
-ContactsBtn.addEventListener("click", () => {
-  window.location.href = "contacts.html";
-});
-const LearnMoreBtn = document.getElementById("LearnMoreBtn");
-LearnMoreBtn.addEventListener("click", () => {
-  window.location.href = "services.html";
-});
+const AboutUsBtnRu = document.getElementById("AboutUsBtnRu");
+if (AboutUsBtnRu)
+  AboutUsBtnRu.addEventListener("click", () => {
+    window.location.href = "indexRu.html";
+  });
+
+const ServicesBtnRu = document.getElementById("ServicesBtnRu");
+if (ServicesBtnRu)
+  ServicesBtnRu.addEventListener("click", () => {
+    window.location.href = "servicesRu.html";
+  });
+
+const ContactsBtnRu = document.getElementById("ContactsBtnRu");
+if (ContactsBtnRu)
+  ContactsBtnRu.addEventListener("click", () => {
+    window.location.href = "contactsRu.html";
+  });
+
+const LearnMoreBtnRu = document.getElementById("LearnMoreBtnRu");
+if (LearnMoreBtnRu)
+  LearnMoreBtnRu.addEventListener("click", () => {
+    window.location.href = "servicesRu.html";
+  });
+
+const AboutUsBtnBg = document.getElementById("AboutUsBtnBg");
+if (AboutUsBtnBg)
+  AboutUsBtnBg.addEventListener("click", () => {
+    window.location.href = "indexBg.html";
+  });
+
+const ServicesBtnBg = document.getElementById("ServicesBtnBg");
+if (ServicesBtnBg)
+  ServicesBtnBg.addEventListener("click", () => {
+    window.location.href = "servicesBg.html";
+  });
+
+const ContactsBtnBg = document.getElementById("ContactsBtnBg");
+if (ContactsBtnBg)
+  ContactsBtnBg.addEventListener("click", () => {
+    window.location.href = "contactsBg.html";
+  });
+
+const LearnMoreBtnBg = document.getElementById("LearnMoreBtnBg");
+if (LearnMoreBtnBg)
+  LearnMoreBtnBg.addEventListener("click", () => {
+    window.location.href = "servicesBg.html";
+  });
